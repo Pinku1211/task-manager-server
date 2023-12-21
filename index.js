09db6a74f598339b8253e27302335a426115fd8a
Pinku1211 <pinkukumarsarker@gmail.com>
2023-12-22 04:59:56 +0600

    update task status

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+  ],
+  credentials: true
+}));
 app.use(express.json());
-console.log(process.env.DB_PASS)
-
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r8pib.mongodb.net/?retryWrites=true&w=majority`;
@@ -26,8 +29,42 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const taskCollection = client.db('task-management').collection('tasks');
 
+    app.post('/tasks', async (req, res) => {
+      const newTask = req.body;
+      const result = await taskCollection.insertOne(newTask)
+      console.log(result)
+      res.send(result)
+    })
 
+    app.get('/tasks', async (req, res) => {
+      const result = await taskCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.delete('/tasks/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    app.put('/task/:id', async (req, res) => {
+      const id = req.params.id
+      console.log(id)
+      const newStatus = req.body.status
+      console.log(req.body.status)
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const changeStatus = {
+        $set: {
+          "status": newStatus
+        }
+      }
+      const result = await taskCollection.updateOne(filter, changeStatus, options)
+      res.send(result)
+    })
 
     
     // Send a ping to confirm a successful connection
